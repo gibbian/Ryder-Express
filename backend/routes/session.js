438eb55cc1654bf8b680b22db31228c5f409dae1
wlandin <61633686+wlandin@users.noreply.github.com>
2022-04-19 19:22:09 -0500

    file management and file folders created

---
+++ backend/routes/session.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const UserController = require('../controllers/users');
+
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+    try {
+        const body = req.body;
+        
+        const result = await UserController.authenticateUser(body.username, body.password);
+        res.status(201).json(result);
+    } catch (err) {
+        console.error('Failed to create new user:', err);
+        res.status(500).json({ message: err.toString() });
+    }
+
+    next();
+})
+
+module.exports = router;
+
