eabe230708e4023046297ee546e2eb1aabec99f6
Jurech <78240036+Jurech@users.noreply.github.com>
2022-04-21 17:28:15 -0500

    GET employees by company

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -227,6 +227,42 @@ module.exports = function routes(app, logger) {
     });
   });
 
+  // GET /employee by company
+  app.get('/employee/:company', (req, res) => {
+    // obtain a connection from our pool of connections
+    if (!("company" in req.params)){
+      res.status(400).send({
+        success: false,
+        response: "Missing required field: `company`",
+      });
+    }
+    else{
+      pool.getConnection(function (err, connection){
+        if(err){
+          // if there is an issue obtaining a connection, release the connection instance and log the error
+          logger.error('Problem obtaining MySQL connection',err)
+          res.status(400).send('Problem obtaining MySQL connection'); 
+        } else {
+          // if there is no issue obtaining a connection, execute query and release connection
+          connection.query('SELECT * FROM Employee WHERE company_id = ?', req.params.company, function (err, rows, fields) {
+            connection.release();
+            if (err) {
+              logger.error("Error while fetching employees: \n", err);
+              res.status(400).json({
+                "data": [],
+                "error": "Error obtaining employees"
+              })
+            } else {
+              res.status(200).json({
+                "data": rows
+              });
+            }
+          });
+        }
+      });
+    }   
+  });
+
   // GET /delivery
   app.get('/delivery', (req, res) => {
     // obtain a connection from our pool of connections
