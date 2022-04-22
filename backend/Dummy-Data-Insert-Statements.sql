180e8ca98991af17f05e9f07b93c903f019ad3a2
Jurech <78240036+Jurech@users.noreply.github.com>
2022-04-21 19:42:36 -0500

    Update dummy data

--- backend/Dummy-Data-Insert-Statements.sql
+++ backend/Dummy-Data-Insert-Statements.sql
@@ -61,6 +61,10 @@ VALUES
 ('All My Sons', 'ihavetoomanykids@gmail.com', '205-975-3486', 'Deep South', 200, 15, 0, false),
 ('West Coast Elites', 'educatedandstrong@gmail.com', '213-473-9763', 'West Coast', 500, 5, 0, false);
 
+UPDATE Shipper SET bio = 'We love the cold up here!' WHERE id = 1;
+UPDATE Shipper SET bio = 'It is hot, and we are hot!' WHERE id = 2;
+UPDATE Shipper SET bio = 'Gotta love that west coast air!' WHERE id = 3;
+
 CREATE INDEX names
 ON Shipper(name);
 
